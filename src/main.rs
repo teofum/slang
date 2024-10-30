3a34f27c3fa7ebd3e8db5073d8aed4a453f132a5
Teo Fumagalli <teo.fum@gmail.com>
2024-10-30 16:12:32 -0300

    add print number flag

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,29 @@ mod error;
 mod prologue;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut args = env::args().skip(1);
+    let mut args = env::args().skip(1).peekable();
+    
+    let mut print_code = false;
+    if args.peek().is_some_and(|arg| arg == "-p") {
+        args.next();
+        print_code = true;
+    }
+    
     let program_file = File::open(args.next().unwrap())?;
     match Program::from_file(&program_file) {
         Ok(program) => {
-            println!("Program number: {}", program);
-            
-            let mut machine = Machine::new(
-                State::from_vars(args.map(|arg| arg.parse::<usize>().unwrap()).collect()),
-                &program,
-            );
+            if print_code {
+                println!("Program number: {}", program);
+            } else {
+                let mut machine = Machine::new(
+                    State::from_vars(args.map(|arg| arg.parse::<usize>().unwrap()).collect()),
+                    &program,
+                );
 
-            machine.run();
+                machine.run();
 
-            println!("Y = {}", machine.state().get_var(&Variable::Y));
+                println!("Y = {}", machine.state().get_var(&Variable::Y));
+            }
         }
         Err(e) => {
             println!("\x1b[31;1m{}\x1b[0m", e);
